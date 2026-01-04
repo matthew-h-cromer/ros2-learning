b636164e7b5b8e47b2f122d5f125518436e85ed2
Matthew Cromer <matthew.h.cromer@gmail.com>
2026-01-03 21:10:20 -0800

    chore: add gitignore and remove build artifacts from tracking

--- build/py_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/matthew-cromer/Documents/ros2-pubsub-from-scratch/install/py_pubsub'
